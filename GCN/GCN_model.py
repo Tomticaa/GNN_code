985e286de97dfcb7627166940b9d245dc01c9971
Timtic <1497392092@qq.com>
2024-06-26 12:17:49 +0800

    增加数据集处理，模型结构以及模型训练等函数。

---
+++ GCN/GCN_model.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+"""
+Author  ：Jo
+USER    ：JO
+IDE     ：PyCharm 
+File    ：GCN_model.py
+Date    ：2024/6/26 下午12:16 
+Project ：GNN_code 
+PROJECT DESCRIPTION：
+    
+"""
---
+++ GCN/model_train.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+"""
+Author  ：Jo
+USER    ：JO
+IDE     ：PyCharm 
+File    ：model_train.py
+Date    ：2024/6/26 下午12:17 
+Project ：GNN_code 
+PROJECT DESCRIPTION：
+    
+"""
