a875848480630973f9fc3b8fbc191bbcc4738f7a
Timtic <1497392092@qq.com>
2024-06-26 12:00:45 +0800

    Initial commit

---
+++ GCN/dataset_process.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+"""
+Author  ：Jo
+USER    ：JO
+IDE     ：PyCharm
+File    ：dataset_process.py
+Date    ：2024/6/26 11:22
+Project ：GNN_code
+PROJECT DESCRIPTION：
+    用于对cora数据集的预处理
+"""
